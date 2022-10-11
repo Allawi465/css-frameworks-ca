e66125d7743284dcfc3fba6644d158117329cd1c
Mohammed <allawi465@gmail.com>
2022-10-12 01:02:54 +0200

    delete js files

--- src/js/api/endpoint/posts.mjs
+++
@@ -1,5 +0,0 @@
-export const API_GET_Posts = '/posts/';
-export const API_POST_Posts = `/profiles/${name}`;
-export const API_PUT_Posts = `/posts/${id}`;
-export const API_DELETE_Posts = `/posts/${id}`;
-export const API_GET_Posts_Id = `/posts/${id}`;
--- src/js/api/endpoint/profiles.mjs
+++
@@ -1,5 +0,0 @@
-export const API_GET_Profiles = '/profiles/';
-export const Profiles_GET_Names = `/profiles/${name}`;
-export const Profiles_PUT_Follow = `/profiles/${name}/follow`;
-export const Profiles_PUT_Media = `/profiles/${name}/media`;
-export const Profiles_PUT_Unfollow = `/profiles/${name}/unfollow`;
